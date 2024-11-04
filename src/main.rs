8ca4770c7a1e0d9082dcb6d5124bfdd0011d7dd3
ronin <wuyuanhan@asiasoft.hk>
2024-11-04 17:33:12 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,58 @@
+use hex::encode;
 use serialport;
-use std::io::Write;
+use std::io::{self, Read, Write};
 use std::time::Duration;
 
 #[repr(C)]
 struct PackageHeader {
     identify: u16,
-    r#type: u8,
+    r#type: CmdTypeT,
     size: u8,
 }
 
 impl PackageHeader {
-    fn to_bytes(&self) -> [u8; 4] {
+    fn to_bytes(&self) -> [u8; 7] {
         [
             (self.identify >> 8) as u8,
             self.identify as u8,
-            self.r#type,
+            (self.r#type as u32 >> 24) as u8,
+            (self.r#type as u32 >> 16) as u8,
+            (self.r#type as u32 >> 8) as u8,
+            self.r#type as u8,
             self.size,
         ]
     }
 }
 
+#[repr(u32)]
+#[derive(Debug, Clone, Copy)]
+enum CmdTypeT {
+    // CmdConfig = 0,
+    CmdStarSearch = 1,
+    // CmdSearchRes = 2,
+    // CmdLightOn = 3,
+    // CmdLightOff = 4,
+    // CmdTestLedWithGroupLight = 5,
+    // CmdGroupSet = 6,
+    // CmdVflRes = 10,
+    // CmdVfhRes = 11,
+    // CmdIrRes = 12,
+    // CmdScrRes = 13,
+    // CmdVrRes = 14,
+    // CmdShortRes = 15,
+    // CmdCurGroup = 20,
+    // CmdTestEnd = 21,
+    // CmdPassBeep = 22,
+    // CmdNotPassBeep = 23,
+    // CmdGroupCount = 100,
+    // CmdGroupAdd = 101,
+    // CmdTestLed = 102,
+}
+
 fn main() {
     // 设置串口参数
-    let port_name = "/dev/cu.usbmodem1234561"; // 替换为你的串口设备路径
+    let port_name = "/dev/cu.usbmodem1234561";
     let baud_rate = 115200;
-
-    // let settings = serialport::SerialPortBuilder {
-    //     baud_rate: baud_rate,
-    //     data_bits: DataBits::Eight,
-    //     flow_control: FlowControl::None,
-    //     parity: Parity::None,
-    //     stop_bits: StopBits::One,
-    //     timeout: Duration::from_millis(1000),
-    // };
 
     // 打开串口
     match serialport::new(port_name, baud_rate)
@@ -42,13 +62,11 @@ fn main() {
         Ok(mut port) => {
             println!("串口打开成功");
 
-            // // 发送数据
-            // let data_to_send = b"CMD_STAR_SEARCH";
-            const CMD_STAR_SEARCH: u8 = 0x01;
+            // 发送数据
             let header = PackageHeader {
-                identify: 0x544F,        // 设置标识符
-                r#type: CMD_STAR_SEARCH, // 设置命令类型为 CMD_STAR_SEARCH
-                size: 0,                 // 设置数据大小为 0
+                identify: 0x544F,                // 设置标识符
+                r#type: CmdTypeT::CmdStarSearch, // 设置命令类型为 CMD_STAR_SEARCH
+                size: 0,                         // 设置数据大小为 0
             };
             match port.write(&header.to_bytes()) {
                 Ok(_) => println!("数据发送成功"),
@@ -59,7 +77,7 @@ fn main() {
             let mut buffer: Vec<u8> = vec![0; 100];
             match port.read(buffer.as_mut_slice()) {
                 Ok(bytes_read) => {
-                    println!("接收到数据: {:?}", &buffer[..bytes_read]);
+                    println!("接收到数据: {:?}", encode(&buffer[..bytes_read]));
                 }
                 Err(e) => eprintln!("接收数据失败: {:?}", e),
             }
@@ -67,5 +85,3 @@ fn main() {
         Err(e) => eprintln!("打开串口失败: {:?}", e),
     }
 }
-
-// 假设 CMD_STAR_SEARCH 的值为 0x01
